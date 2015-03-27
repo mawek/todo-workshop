58275f415312ad60551fdde9966826c6b31f1168
Marek Gerhart <marek.gerhart@gooddata.com>
2015-03-27 17:24:48 +0100

    lets make it editable

--- app/components/todo-widget/component.js
+++ app/components/todo-widget/component.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  todos: [],
+  newTodoTitle: null,
+
+  actions: {
+    addNewTodo: function () {
+      var newTodoTitle = this.get('newTodoTitle');
+
+      this.get('todos').pushObject(
+        Ember.Object.create({
+          title: newTodoTitle,
+          isCompleted: false
+        })
+      );
+
+      this.set('newTodoTitle', '');
+    }
+  }
 });
