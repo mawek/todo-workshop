30554afcefd60b2ca34e62a3b40527d324a43a88
Marek Gerhart <marek.gerhart@gooddata.com>
2015-03-28 12:38:03 +0100

    completed checkbox

--- app/components/todo-list-item/component.js
+++ app/components/todo-list-item/component.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
-  classNames: ['todo-list-item'],
-  
+  classNameBindings: ['todo.isCompleted:completed', ':todo-list-item'],
+
   actions: {
     removeTodo: function (todo) {
       this.sendAction('removeTodo', todo);
